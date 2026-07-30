6d7a2cb1a32aa73d95bcdd64263f5bc842cac9a4
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-07-30 19:33:26 +0530

    Initial Commit

---
+++ OpenQuartz/OpenQuartzApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenQuartzApp.swift
+//  OpenQuartz
+//
+//  Created by Abhiram Krishna M on 30/07/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenQuartzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
