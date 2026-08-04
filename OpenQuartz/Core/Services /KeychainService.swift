6d3350c2230bbcf866b58315ed0fb8343c23518e
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-08-04 18:14:22 +0530

    query to get data

--- OpenQuartz/Core/Services /KeychainService.swift
+++ OpenQuartz/Core/Services /KeychainService.swift
@@ -7,9 +7,33 @@
 
 import Foundation
 
+enum KeychainError: Error {
+    case notFound
+    case unexpectedData
+}
 
 struct KeychainService {
     func readClaudeCredentials() throws -> Data {
         
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: "Claude Code-credentials",
+            kSecAttrAccount as String: "abhiramrkishnam",
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        guard status == errSecSuccess else {
+            throw KeychainError.notFound
+        }
+        
+        guard let data = result as? Data else{
+            throw KeychainError.unexpectedData
+        }
+        
+        return data
     }
 }
