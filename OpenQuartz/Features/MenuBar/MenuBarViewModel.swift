f7bac93535df1f369c9dd8e47fc80d404b91ebb8
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2026-08-06 20:03:08 +0530

    session value stor

---
+++ OpenQuartz/Features/MenuBar/MenuBarViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  MenuBarViewModel.swift
+//  OpenQuartz
+//
+//  Created by Abhiram Krishna M on 06/08/26.
+//
+
+import Foundation
+
+@Observable
+class MenubarViewModel {
+    var sessionPercent: Double = 0
+    
+    func refreshUsage() async{
+        do{
+            let keychain = KeychainService()
+            let data = try keychain.readClaudeCredentials()
+            let decoder = JSONDecoder()
+            let credentials = try decoder.decode(ClaudeCredentials.self, from: data)
+            
+            let usageService = ClaudeUsageService()
+            let usage = try await usageService.fetchUsage(accessToken: credentials.claudeAiOauth.accessToken)
+            
+            sessionPercent = usage.fiveHour?.utilization ?? 0
+        }catch {
+            
+        }
+    }
+}
